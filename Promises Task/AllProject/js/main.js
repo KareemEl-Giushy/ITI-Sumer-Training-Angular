6f42b7f36ee6083a5b3ce98b6d74bae5d36dd4a5
Kareem El-Giushy <kareem.64123@gmail.com>
2023-07-29 09:08:35 +0300

    Adding Promises Session Tasks

---
+++ Promises Task/AllProject/js/main.js
@@ -0,0 +1,41 @@
+class Task {
+    constructor(name, date, status) {
+      this.name = name;
+      this.date = date;
+      this.status = status;
+      this.evaluation = "Incomplete";
+    }
+    
+    updateTask(status) {
+      this.status = status;
+    }
+}
+  
+class ToDoList {
+    constructor() {
+        this.tList = [];
+    }
+
+    addTask(task) {
+        this.tList.append(task);
+    }
+
+    show() {
+        console.log(this.tList);
+    }
+
+    sortByEvaluation(a, b) {
+        return b.evaluation - a.evaluation;
+    }
+
+    evaluateDay(date, evalValue) {
+        for (let todo of this.tList) {
+            if (todo.date === date) {
+                todo.evaluation = evalValue;
+            }
+        }
+        this.tList.sort(this.sortByEvaluation);
+    }
+}
+
+
---
+++ Promises Task/TaskCheck.js
@@ -0,0 +1,29 @@
+let isTaskDone = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(task.status === "done");
+    }, 2000);
+  });
+
+
+function check() {
+    isTaskDone.then((isDone) => {
+        if (isDone) {
+          console.log("Good Job");
+        } else {
+          console.log("This task not done");
+          setTimeout(check, 10000); 
+        }
+      }).catch((error) => {
+        console.error("Error occurred while checking task status:", error);
+      });
+}
+
+
+const task = {
+    taskId: 123,
+    description: "Sample task",
+    status: "in progress",
+};
+
+  
+check(task);
